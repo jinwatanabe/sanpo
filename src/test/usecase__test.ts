ada3f30c6802c6e9f98fc76c48d03f7c6e3cb539
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-04-13 08:49:59 +0900

    add usecaste test file

--- src/test/usecase__test.ts
+++ src/test/usecase__test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.65.0/testing/asserts.ts";
 
-Deno.test("sample test", () => {
+Deno.test("usecase", () => {
   const x = 1 + 2;
   assertEquals(x, 3, "1 + 2 should be 3");
 });
